8b665ecc6b5a2926816ed39e0a08b1be0b69cb4b
dcyuq <ajstephenrosillo1@gmail.com>
2026-09-11 16:35:52 +0800

    dshhf

--- cogs/confirmation.py
+++ cogs/confirmation.py
@@ -11,7 +11,7 @@ from discord import app_commands
 from discord.ext import commands
  
 import embeds
-import confirmation
+from . import confirmation
 from prefixes import display_prefix
 import emojiutils
 from storage import Store, IntKeyStore
