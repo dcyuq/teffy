c4ea607476accc1e338d603d4ca2872821904bb9
dcyuq <ajstephenrosillo1@gmail.com>
2026-09-11 17:03:36 +0800

    dshhf

--- cogs/ticket.py
+++ cogs/ticket.py
@@ -11,7 +11,7 @@ from discord import app_commands
 from discord.ext import commands
  
 import embeds
-from . import confirmation
+import confirmation
 from prefixes import display_prefix
 import emojiutils
 from storage import Store, IntKeyStore
